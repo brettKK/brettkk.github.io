bf04ebb09690c651c89dce20b7eed0313c002094
brettkk <1099446733@qq.com>
2022-11-04 11:36:31 +0800

    go graph

--- posts/os/nju-os/fork_test.c
+++ posts/os/nju-os/fork_test.c
@@ -6,7 +6,12 @@ int main(){
     pid_t pid2 = fork();
     pid_t pid3 = fork();
     printf("hello world from (%d, %d, %d)\n", pid1, pid2, pid3);
+
+    if pid1 == 7 {
+        
+    }
     return 0;
+
 }
 
 /*
